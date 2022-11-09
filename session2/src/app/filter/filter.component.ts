600c26435c2c59f1d97509adffbe471f775a9e25
argun8394 <argundoganali@gmail.com>
2022-11-09 16:49:51 +0300

    Custom Event Binding [@Output]

--- session2/src/app/filter/filter.component.ts
+++ session2/src/app/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -10,9 +10,22 @@ export class FilterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.selectedRadioButtonValue)
   }
   @Input('total') all : number =0;//alias name kullanıldığında yani takma ad pas edilmeli
   @Input() free : number = 0;
   @Input() premium : number = 0;
+
+  selectedRadioButtonValue : string = 'All';
+
+  @Output()
+  filterRadioButtonSelectionChanged : EventEmitter<string> = new EventEmitter<string>();
+
+  onRadioButtonSelectionChanged() {
+    this.filterRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
+    // console.log(this.selectedRadioButtonValue)
+  }
+
+
 
 }
