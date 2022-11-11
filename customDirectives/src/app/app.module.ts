9fd446b0b7dca658dd610d49ab448103fdabd19c
argun8394 <argundoganali@gmail.com>
2022-11-12 00:35:46 +0300

    renderer2

--- customDirectives/src/app/app.module.ts
+++ customDirectives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SetBackgroundDirective } from './CustomDirective/setbackground.directive';
+import { HighlightDirective } from './CustomDirective/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SetBackgroundDirective,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule
